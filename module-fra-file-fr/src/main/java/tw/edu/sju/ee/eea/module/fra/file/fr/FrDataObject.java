bae1f6487bb20f48d3e44ef179de7e9a46cda311
薛聿明 <102m05008@stud.sju.edu.tw>
2014-02-15 15:36:45 +0800

    update FRA Config, use NbBundle.Messages.

--- module-fra-file-fr/src/main/java/tw/edu/sju/ee/eea/module/fra/file/fr/FrDataObject.java
+++ module-fra-file-fr/src/main/java/tw/edu/sju/ee/eea/module/fra/file/fr/FrDataObject.java
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
@@ -27,7 +28,9 @@ import org.openide.nodes.PropertySupport;
 import org.openide.nodes.Sheet;
 import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
+import org.openide.util.NbBundle;
 import org.openide.util.NbBundle.Messages;
+import tw.edu.sju.ee.eea.core.frequency.response.FrequencyResponseConfig;
 
 @Messages({
     "LBL_Fr_LOADER=Files of Fr"
@@ -128,7 +131,7 @@ public class FrDataObject extends MultiDataObject implements OpenCookie {
             }
         }
     }
-    
+
     public FrequencyResponseFile getFile() {
         return this.file;
     }
@@ -155,65 +158,30 @@ public class FrDataObject extends MultiDataObject implements OpenCookie {
         @Override
         protected Sheet createSheet() {
             Sheet.Set set = new Sheet.Set();
-            set.setName("Configuration");
-            set.put(new PropertySupport.ReadOnly("generateChannel", String.class, "generateChannel", "generateChannel") {
+            set.setName(NbBundle.getMessage(FrequencyResponseConfig.class, "name"));
+            String name[] = new String[]{"GenerateDevice", "GenerateChannel", "ResponseDevice", "ResponseChannel",
+                "Voltage", "StartFrequency", "StopFrequrncy", "Points", "RatePerHz", "Description"};
+            for (int i = 0; i < name.length; i++) {
+                String displayName = NbBundle.getMessage(FrequencyResponseConfig.class, "LBL_" + name[i]);
+                String description = NbBundle.getMessage(FrequencyResponseConfig.class, "DCT_" + name[i]);
+                try {
+                    final Method method = FrequencyResponseConfig.class.getMethod("get" + name[i]);
+                    set.put(new PropertySupport.ReadOnly(name[i], method.getReturnType(), displayName, description) {
 
-                @Override
-                public String getValue() throws IllegalAccessException, InvocationTargetException {
-                    return file.getConfig().getGenerateDevice();
+                        @Override
+                        public Object getValue() throws IllegalAccessException, InvocationTargetException {
+                            return method.invoke(file.getConfig());
+                        }
+                    });
+                } catch (NoSuchMethodException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (SecurityException ex) {
+                    Exceptions.printStackTrace(ex);
                 }
-            });
-            set.put(new PropertySupport.ReadOnly("responseChannel", String.class, "responseChannel", "responseChannel") {
-
-                @Override
-                public String getValue() throws IllegalAccessException, InvocationTargetException {
-                    return file.getConfig().getResponseDevice();
-                }
-            });
-            set.put(new PropertySupport.ReadOnly("voltage", Double.class, "voltage", "voltage") {
-
-                @Override
-                public Double getValue() throws IllegalAccessException, InvocationTargetException {
-                    return file.getConfig().getVoltage();
-                }
-            });
-            set.put(new PropertySupport.ReadOnly("minFrequency", Double.class, "minFrequency", "minFrequency") {
-
-                @Override
-                public Double getValue() throws IllegalAccessException, InvocationTargetException {
-                    return file.getConfig().getMinFrequency();
-                }
-            });
-            set.put(new PropertySupport.ReadOnly("maxFrequrncy", Double.class, "maxFrequrncy", "maxFrequrncy") {
-
-                @Override
-                public Double getValue() throws IllegalAccessException, InvocationTargetException {
-                    return file.getConfig().getMaxFrequrncy();
-                }
-            });
-            set.put(new PropertySupport.ReadOnly("length", Integer.class, "length", "length") {
-
-                @Override
-                public Integer getValue() throws IllegalAccessException, InvocationTargetException {
-                    return file.getConfig().getLength();
-                }
-            });
+            }
             Sheet sheet = super.createSheet();
             sheet.put(set);
             return sheet;
         }
     }
-
-//    @MultiViewElement.Registration(
-//        displayName = "#LBL_Fr_EDITOR",
-//        iconBase = "tw/edu/sju/ee/eea/module/fra/file/fr/fr.png",
-//        mimeType = "application/x-plot",
-//        persistenceType = TopComponent.PERSISTENCE_ONLY_OPENED,
-//        preferredID = "Fr",
-//        position = 1000
-//    )
-//    @Messages("LBL_Fr_EDITOR=Source")
-//    public static MultiViewEditorElement createEditor(Lookup lkp) {
-//        return new MultiViewEditorElement(lkp);
-//    }
 }
