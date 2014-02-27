6041ed02c1bb5b08acb41abb90df04f3475061ef
薛聿明 <102m05008@stud.sju.edu.tw>
2014-02-28 01:43:46 +0800

    use lib jni-modinst to list devices.

--- module-fra-file-fr/src/main/java/tw/edu/sju/ee/eea/module/fra/file/fr/FrConfigVisualPanel.java
+++ module-fra-file-fr/src/main/java/tw/edu/sju/ee/eea/module/fra/file/fr/FrConfigVisualPanel.java
@@ -5,9 +5,12 @@
  */
 package tw.edu.sju.ee.eea.module.fra.file.fr;
 
+import java.util.ArrayList;
+import java.util.List;
 import tw.edu.sju.ee.eea.core.frequency.response.FrequencyResponseConfig;
 import javax.swing.JPanel;
 import org.openide.util.NbBundle.Messages;
+import tw.edu.sju.ee.eea.jni.modinst.NIModinstUtils;
 
 @Messages({
     "FrVisualPanel1_displayName=Frequency Response Configuration"
@@ -28,12 +31,37 @@ public final class FrConfigVisualPanel extends JPanel {
 
     public FrequencyResponseConfig getConfig() {
         return new FrequencyResponseConfig(
-                cmbGenerateDevice.getSelectedItem().toString(), txtGenerateChannel.getText(),
-                cmbResponseDevice.getSelectedItem().toString(), txtResponseChanel.getText(),
+                ((Device) cmbGenerateDevice.getSelectedItem()).name, txtGenerateChannel.getText(),
+                ((Device) cmbResponseDevice.getSelectedItem()).name, txtResponseChanel.getText(),
                 Double.parseDouble(txtVoltage.getText()),
                 Double.parseDouble(txtStartFrequency.getText()), Double.parseDouble(txtStopFrequency.getText()),
                 Integer.parseInt(txtPoints.getText()), Integer.parseInt(txtRatePerHz.getText()),
                 txaDescription.getText());
+    }
+
+    private Object[] getDevice(String driver) {
+        List<NIModinstUtils.Device> devices = NIModinstUtils.list(driver);
+        List<Device> list = new ArrayList();
+        for (int i = 0; i < devices.size(); i++) {
+            list.add(new Device(devices.get(i)));
+        }
+        return list.toArray();
+    }
+
+    private class Device {
+
+        private String name;
+        private String model;
+
+        public Device(NIModinstUtils.Device device) {
+            name = device.getDeviceName();
+            model = device.getDeviceModel();
+        }
+
+        @Override
+        public String toString() {
+            return name + " - " + model;
+        }
     }
 
     /**
@@ -88,32 +116,50 @@ public final class FrConfigVisualPanel extends JPanel {
 
         org.openide.awt.Mnemonics.setLocalizedText(lblDescription, org.openide.util.NbBundle.getMessage(FrConfigVisualPanel.class, "FrConfigVisualPanel.lblDescription.text")); // NOI18N
 
-        cmbGenerateDevice.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "PXI1Slot3", "PXI1Slot4" }));
-        cmbGenerateDevice.setPreferredSize(new java.awt.Dimension(101, 21));
+        cmbGenerateDevice.setModel(new javax.swing.DefaultComboBoxModel(getDevice("niFgen")));
+        cmbGenerateDevice.setMaximumSize(new java.awt.Dimension(201, 21));
+        cmbGenerateDevice.setMinimumSize(new java.awt.Dimension(101, 21));
+        cmbGenerateDevice.setPreferredSize(new java.awt.Dimension(151, 21));
 
         txtGenerateChannel.setText(org.openide.util.NbBundle.getMessage(FrConfigVisualPanel.class, "FrConfigVisualPanel.txtGenerateChannel.text")); // NOI18N
-        txtGenerateChannel.setPreferredSize(new java.awt.Dimension(101, 21));
+        txtGenerateChannel.setMaximumSize(new java.awt.Dimension(101, 21));
+        txtGenerateChannel.setMinimumSize(new java.awt.Dimension(31, 21));
+        txtGenerateChannel.setPreferredSize(new java.awt.Dimension(51, 21));
 
-        cmbResponseDevice.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "PXI1Slot4" }));
-        cmbResponseDevice.setPreferredSize(new java.awt.Dimension(101, 21));
+        cmbResponseDevice.setModel(new javax.swing.DefaultComboBoxModel(getDevice("niScope")));
+        cmbResponseDevice.setMaximumSize(new java.awt.Dimension(201, 21));
+        cmbResponseDevice.setMinimumSize(new java.awt.Dimension(101, 21));
+        cmbResponseDevice.setPreferredSize(new java.awt.Dimension(151, 21));
 
         txtResponseChanel.setText(org.openide.util.NbBundle.getMessage(FrConfigVisualPanel.class, "FrConfigVisualPanel.txtResponseChanel.text")); // NOI18N
-        txtResponseChanel.setPreferredSize(new java.awt.Dimension(101, 21));
+        txtResponseChanel.setMaximumSize(new java.awt.Dimension(101, 21));
+        txtResponseChanel.setMinimumSize(new java.awt.Dimension(31, 21));
+        txtResponseChanel.setPreferredSize(new java.awt.Dimension(51, 21));
 
         txtVoltage.setText(org.openide.util.NbBundle.getMessage(FrConfigVisualPanel.class, "FrConfigVisualPanel.txtVoltage.text")); // NOI18N
-        txtVoltage.setPreferredSize(new java.awt.Dimension(101, 21));
+        txtVoltage.setMaximumSize(new java.awt.Dimension(201, 21));
+        txtVoltage.setMinimumSize(new java.awt.Dimension(101, 21));
+        txtVoltage.setPreferredSize(new java.awt.Dimension(151, 21));
 
         txtStartFrequency.setText(org.openide.util.NbBundle.getMessage(FrConfigVisualPanel.class, "FrConfigVisualPanel.txtStartFrequency.text")); // NOI18N
-        txtStartFrequency.setPreferredSize(new java.awt.Dimension(101, 21));
+        txtStartFrequency.setMaximumSize(new java.awt.Dimension(201, 21));
+        txtStartFrequency.setMinimumSize(new java.awt.Dimension(101, 21));
+        txtStartFrequency.setPreferredSize(new java.awt.Dimension(151, 21));
 
         txtStopFrequency.setText(org.openide.util.NbBundle.getMessage(FrConfigVisualPanel.class, "FrConfigVisualPanel.txtStopFrequency.text")); // NOI18N
-        txtStopFrequency.setPreferredSize(new java.awt.Dimension(101, 21));
+        txtStopFrequency.setMaximumSize(new java.awt.Dimension(201, 21));
+        txtStopFrequency.setMinimumSize(new java.awt.Dimension(101, 21));
+        txtStopFrequency.setPreferredSize(new java.awt.Dimension(151, 21));
 
         txtPoints.setText(org.openide.util.NbBundle.getMessage(FrConfigVisualPanel.class, "FrConfigVisualPanel.txtPoints.text")); // NOI18N
-        txtPoints.setPreferredSize(new java.awt.Dimension(101, 21));
+        txtPoints.setMaximumSize(new java.awt.Dimension(101, 21));
+        txtPoints.setMinimumSize(new java.awt.Dimension(31, 21));
+        txtPoints.setPreferredSize(new java.awt.Dimension(51, 21));
 
         txtRatePerHz.setText(org.openide.util.NbBundle.getMessage(FrConfigVisualPanel.class, "FrConfigVisualPanel.txtRatePerHz.text")); // NOI18N
-        txtRatePerHz.setPreferredSize(new java.awt.Dimension(101, 21));
+        txtRatePerHz.setMaximumSize(new java.awt.Dimension(101, 21));
+        txtRatePerHz.setMinimumSize(new java.awt.Dimension(31, 21));
+        txtRatePerHz.setPreferredSize(new java.awt.Dimension(51, 21));
 
         txaDescription.setColumns(20);
         txaDescription.setRows(5);
@@ -131,27 +177,27 @@ public final class FrConfigVisualPanel extends JPanel {
                     .addComponent(lblGenerateDevice)
                     .addComponent(lblStartFrequency)
                     .addComponent(lblVoltage)
-                    .addComponent(lblPoints)
-                    .addComponent(lblDescription))
+                    .addComponent(lblDescription)
+                    .addComponent(lblStopFrequrncy))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(cmbGenerateDevice, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(cmbGenerateDevice, 0, 155, Short.MAX_VALUE)
                             .addComponent(cmbResponseDevice, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(txtVoltage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(txtStartFrequency, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(txtPoints, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(txtStopFrequency, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(30, 30, 30)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lblGenerateChannel)
                             .addComponent(lblResponseChannel)
-                            .addComponent(lblStopFrequrncy)
-                            .addComponent(lblRatePerHz))
+                            .addComponent(lblRatePerHz)
+                            .addComponent(lblPoints))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtRatePerHz, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(txtStopFrequency, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(txtPoints, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(txtRatePerHz, javax.swing.GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)
                             .addComponent(txtResponseChanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(txtGenerateChannel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addComponent(scrDescription))
@@ -180,12 +226,12 @@ public final class FrConfigVisualPanel extends JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
                     .addComponent(lblStartFrequency)
                     .addComponent(txtStartFrequency, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lblStopFrequrncy)
-                    .addComponent(txtStopFrequency, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(lblPoints, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtPoints, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
-                    .addComponent(lblPoints, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtPoints, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblStopFrequrncy)
+                    .addComponent(txtStopFrequency, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lblRatePerHz)
                     .addComponent(txtRatePerHz, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
